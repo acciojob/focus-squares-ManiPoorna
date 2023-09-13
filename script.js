b2a90cd25a6ae287fd6552a7e34e8864a74394ea
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-13 13:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+
+const squares = document.getElementsByClassName("square");
+// console.log(squares)
+
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", (e) => {
+        const id = e.target.id
+        console.log(id)
+        for (let i = 0; i < squares.length; i++) {
+            if(squares[i].id !== id){
+                squares[i].style.backgroundColor = "#6F4E37";
+            }            
+        }
+    });
+    for (let j = 0; j < squares.length; j++) {
+        squares[j].addEventListener('mouseout',()=>{
+            squares[i].style.backgroundColor = "#E6E6FA"
+        })        
+    }
+}
